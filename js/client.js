666b1dbe55b94d09dd1ae9465d36b2d06bc63cd7
Singh <ritulsingh123700@gmail.com>
2024-01-20 22:55:03 +0530

    some changes in css

--- js/client.js
+++ js/client.js
@@ -1,9 +1,13 @@
-const socket=io('http://localhost:8000');
+const socket=io('http://localhost:8000')
 
+// get DOM elements in the respective js variables
 const form=document.getElementById('send-container');
 const msgInp=document.getElementById('msgInput')
 const msgContainer=document.querySelector(".container")
+// Audio that will play on recieving message
 var audio=new Audio('tone.mp3');
+
+// function ehich will append event info to the container
 const append=(message,position)=>{
     const msgElement=document.createElement('div');
     msgElement.innerText=message;
@@ -15,6 +19,7 @@ const append=(message,position)=>{
     }
 }
 
+// if the form get submitted then send message to the server
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const msg=msgInp.value;
@@ -23,15 +28,19 @@ form.addEventListener('submit',(e)=>{
     msgInp.value='';
 })
 
+//ask new user for their name and let the server know
 const name=prompt("Enter your name to join ");
 socket.emit('new-user-joined',name);
 
+// if new uswe joins, recievetheir name from the server
 socket.on('user-joined',name=>{
     append(`${name} joined the chat`,'right')
 })
+// if server send a message , recieve it
 socket.on('recieve',data=>{
     append(`${data.name}:${data.message}`,'left')
 })
+// if a user leaves the chat , apaaend the info to the container
 socket.on('left',name=>{
     append(`${name} left the chat`,'left')
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -2,13 +2,16 @@ const io=require('socket.io')(8000)
 const users={}
 
 io.on('connection',socket=>{
+    // if any new user joined,let other user connected to the server know!
     socket.on('new-user-joined',name=>{
         // console.log("new user",name);
         users[socket.id]=name;
         socket.broadcast.emit('user-joined',name);
     });
+    //if someone sends the message,broadcast it to other people
     socket.on('send',message=>{
         socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
+    // if someone leaves the chat , let other know
     });
     socket.on('disconnect',message=>{
         socket.broadcast.emit('left',users[socket.id])
